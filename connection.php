cb167aec2f67200d24047ada5fe7f072fc1ebf80
Mohamedh618 <155271494+Mohamedh618@users.noreply.github.com>
2025-08-20 19:04:28 +0300

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,41 +1,15 @@
-<!-- <?php
-$host = "mysql.railway.internal";// من Railway
-$port = 3306; // البورت من Railway
-$dbname = "railway"; 
-$user = "root"; 
-$password = "lMLyVHBHrPGvMOxMofJjwZVCOavcNpwV"; // الباسورد من Railway
-
-// الاتصال
-$conn = new mysqli($host, $user, $password, $dbname, $port);
-
-// التحقق من الاتصال
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-} 
-echo "✅ Connected successfully";
-?> -->
-
-
 <?php
-$host = "yamabiko.proxy.rlwy.net";
-$port = 35052;
-$user = "root";
-$password = "lMLyVHBHrPGvMOxMofJjwZVCOavcNpwV";
-$database = "railway";
+$host = 'nozomi.proxy.rlwy.net';
+$user = 'root';
+$pass = 'BidkvecOfSssXmdZfLtdjwBwxbyCuiYP';
+$db   = 'railway';
+$port = 46884;
 
-// الاتصال
-$conn = new mysqli($host, $user, $password, $database, $port);
+$conn = new mysqli($host, $user, $pass, $db, $port);
 
-// تحقق من الاتصال
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
-echo "✅ Connected successfully";
 
-// مثال: استعلام
-$result = $conn->query("SELECT NOW() as current_time");
-$row = $result->fetch_assoc();
-echo "<br>Server time: " . $row['current_time'];
-
-$conn->close();
+echo "Connected to Railway MySQL successfully!";
 ?>
